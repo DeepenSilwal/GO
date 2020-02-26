d6036917a01732f298d635049c9e8631b8bca6a0
DSilwal <deepen.silwal@ttu.edu>
2020-02-26 00:25:54 -0600

    Sign in and Sing up connected with Firebase authentication and is functional, User Portal is added to Navigation Page

--- app/src/main/java/com/example/myapp/SecondActivity.java
+++ app/src/main/java/com/example/myapp/SecondActivity.java
@@ -1,8 +1,11 @@
 package com.example.myapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 //import android.support.v4.app.FragmentActivity;
 
 import androidx.fragment.app.FragmentActivity;
@@ -14,30 +17,44 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class SecondActivity extends FragmentActivity implements OnMapReadyCallback {
+public class  SecondActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
     private Button ClaimAddress;
     private EditText StartLocation;
     private EditText EndLocation;
+    private ImageButton profilebutton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+
+
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
+        profilebutton = (ImageButton)findViewById(R.id.imageButton);
         mMap = googleMap;
         // Add a marker in Kathmandu, Nepal, and move the camera.
-        LatLng Nepal = new LatLng(27.610504, 85.357820);
+        LatLng Nepal = new LatLng(27.605064, 85.363617);
         mMap.addMarker(new MarkerOptions().position(Nepal).title("Marker in Kathmandu"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(Nepal));
         mMap.setMinZoomPreference(14);
         mMap.setMapType(mMap.MAP_TYPE_HYBRID);
+
+        profilebutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(SecondActivity.this, ThirdActivity.class));
+            }
+        });
     }
 }
--- app/src/main/java/com/example/myapp/SignUp.java
+++ app/src/main/java/com/example/myapp/SignUp.java
@@ -1,5 +1,6 @@
 package com.example.myapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -7,6 +8,15 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.util.Log;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class SignUp extends AppCompatActivity {
 
@@ -15,35 +25,78 @@ public class SignUp extends AppCompatActivity {
     private EditText Password;
     private EditText ConfirmPassword;
     private Button SignUp;
+    private FirebaseAuth mAuth;
+    private static final String TAG = "";
+    private TextView SignIn;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
-
+        mAuth = FirebaseAuth.getInstance();
         FullName = (EditText)findViewById(R.id.fullname);
         Email = (EditText)findViewById(R.id.etEmail);
         Password = (EditText)findViewById(R.id.etPassword);
         ConfirmPassword = (EditText)findViewById(R.id.ConfirmPassword);
         SignUp = (Button)findViewById(R.id.SignUp);
+        SignIn = (TextView)findViewById(R.id.signin);
 
         SignUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                validate();
+                String emailID = Email.getText().toString();
+                String paswd = Password.getText().toString();
+                String fullname = FullName.getText().toString();
+                String confpaswd = ConfirmPassword.getText().toString();
+                if(emailID.isEmpty()){
+                    Email.setError("Please Provide Your Email");
+                    Email.requestFocus();
+                }
+                else if(paswd.isEmpty()){
+                    Password.setError("Please Provide Your Password");
+                    Password.requestFocus();
+                }
+                else if(fullname.isEmpty()){
+                    FullName.setError(("Please Provide Your Full Name"));
+                    FullName.requestFocus();
+                }
+                else if(confpaswd.isEmpty()){
+                    ConfirmPassword.setError("Please Confirm Your Password");
+                    ConfirmPassword.requestFocus();
+                }
+                else if(!(paswd.equals(confpaswd))){
+                    Toast.makeText(SignUp.this, "Password Does Not Match", Toast.LENGTH_SHORT).show();
+                }
+                else if (!(emailID.isEmpty() && paswd.isEmpty() && fullname.isEmpty() && confpaswd.isEmpty())){
+                    mAuth.createUserWithEmailAndPassword(emailID,paswd).addOnCompleteListener(SignUp.this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                           if (!task.isSuccessful()){
+                               Toast.makeText(SignUp.this.getApplicationContext(), "SignUp Unsuccessfull: " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                           }
+                           else{
+                               Log.d(TAG, "createUserWithEmail:success");
+                               FirebaseUser user = mAuth.getCurrentUser();
+                               startActivity(new Intent(SignUp.this, MainActivity.class));
+                           }
+                        }
+                    });
+                }
+                else{
+                    Toast.makeText(SignUp.this, "Error", Toast.LENGTH_SHORT).show();
+                }
+
 
             }
-
-
+        });
+        SignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent (SignUp.this, MainActivity.class));
+            }
         });
 
     }
-
-    private void validate(){
-
-            Intent intent = new Intent(SignUp.this, SecondActivity.class);
-            startActivity(intent);
-
-    }
+    
 }
